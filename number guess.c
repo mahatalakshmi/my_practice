d8a20a3706cb76d3b4cf8bd171cfde7b07c48683
mahatalakshmi <91476640+mahatalakshmi@users.noreply.github.com>
2021-10-09 09:49:33 +0530

    Update number guess.c

--- number guess.c
+++ number guess.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<math.h>
 void guess(int N)
 {
@@ -9,7 +10,7 @@ void guess(int N)
        if(nog>9)
         {
             printf("\nyou loose\n");
-            break;
+            exit(0);
         }
         scanf("%d",&guess);
         if(guess>n&&guess<N)
@@ -26,6 +27,7 @@ void guess(int N)
         {
             printf("you guess correct nof guesses: %d the number is : %d",nog,n);
             nog++;
+            exit(0);
         }
         else printf("enter in the range of 1-%d",N); 
     }while (guess!=n);
