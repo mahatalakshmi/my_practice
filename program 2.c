fd036fab9144c3beb9929d4a7f1e9b5619b47c76
mahatalakshmi <91476640+mahatalakshmi@users.noreply.github.com>
2022-07-02 15:28:36 +0530

    Delete program 2.c

--- program 2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-extern int count;
-void some()
-{
-    count=5;
-    printf("%d",count);
-}
-
-OFFICE OF THE ADDL.COMMISSIONER OF INCOMETAX, DEEPTHI TOWERS, MAIN ROAD, KAKINADA- 533 004
